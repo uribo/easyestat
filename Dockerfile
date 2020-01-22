6ece26199282044f60181f381c9c452c9eeb95c5
Shinya Uryu <suika1127@gmail.com>
2020-01-23 07:38:07 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM rocker/rstudio:3.6.2
+
+RUN set -x && \
+  apt-get update && \
+  apt-get install -y --no-install-recommends \
+    libmagick++-dev && \
+  apt-get clean && \
+  rm -rf /var/lib/apt/lists/*
+
+ARG GITHUB_PAT
+
+RUN set -x && \
+  echo "GITHUB_PAT=$GITHUB_PAT" >> /usr/local/lib/R/etc/Renviron
+
+RUN set -x && \
+  install2.r --error --skipinstalled --repos 'http://mran.revolutionanalytics.com/snapshot/2020-01-15' \
+    renv && \
+  rm -rf /tmp/downloaded_packages/ /tmp/*.rds
---
+++ R/stat_map.R
@@ -0,0 +1,30 @@
+#' Download Japan prefecture map from e-stat
+#' @param prefcode jis code for prefecture and city identifical number. If prefecture, must be from 1 to 47.
+#' @param dest If TRUE, to unzip downloaded files.
+#' @inheritParams utils::unzip
+#' @export
+download_stat_map <- function(prefcode, exdir = ".", dest = TRUE) {
+  prefcode <-
+    stringr::str_pad(prefcode, width = 2, pad = "0")
+  rlang::arg_match(prefcode, values = stringr::str_pad(seq.int(47), width = 2, pad = "0"))
+  x <-
+    glue::glue(
+      "https://www.e-stat.go.jp/gis/statmap-search/data?dlserveyId=A002005212015&code={prefcode}&coordSys=1&format=shape&downloadType=5"
+    )
+  qry <-
+    purrr::pluck(httr::parse_url(x), "query")
+  destfile <-
+    glue::glue(
+      "{exdir}/{serveyId}{coordSys}{prefcode}.zip",
+      serveyId = qry$dlserveyId,
+      coordSys = dplyr::case_when(qry$coordSys == "1" ~ "DDSWC")
+    )
+  utils::download.file(url = x,
+                       destfile = destfile)
+  if (dest == TRUE) {
+    utils::unzip(
+      zipfile = destfile,
+      exdir = glue::glue("{tools::file_path_sans_ext(destfile)}")
+    )
+  }
+}
