60e526d154dff4978f3195256789716350ada812
Shinya Uryu <uryu.shinya@nies.go.jp>
2020-03-31 19:08:29 +0900

    Tweaks

--- R/stat_map.R
+++ R/stat_map.R
@@ -1,15 +1,16 @@
 #' Download Japan prefecture map from e-stat
-#' @param prefcode jis code for prefecture and city identifical number. If prefecture, must be from 1 to 47.
+#' @param prefcode The JIS-code for prefecture and city identifical number.
+#' If prefecture, must be from 1 to 47.
 #' @param dest If TRUE, to unzip downloaded files.
 #' @inheritParams utils::unzip
 #' @export
-download_stat_map <- function(prefcode, exdir = ".", dest = TRUE) {
+download_stat_map <- function(prefcode, exdir = ".", dest = TRUE, .survey_id = c("A002005212015")) {
   prefcode <-
     stringr::str_pad(prefcode, width = 2, pad = "0")
   rlang::arg_match(prefcode, values = stringr::str_pad(seq.int(47), width = 2, pad = "0"))
   x <-
     glue::glue(
-      "https://www.e-stat.go.jp/gis/statmap-search/data?dlserveyId=A002005212015&code={prefcode}&coordSys=1&format=shape&downloadType=5"
+      "https://www.e-stat.go.jp/gis/statmap-search/data?dlserveyId={.survey_id}&code={prefcode}&coordSys=1&format=shape&downloadType=5"
     )
   qry <-
     purrr::pluck(httr::parse_url(x), "query")
