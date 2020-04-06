ae9f10933238ea8df801da562d3ef5551deb5ad9
Shinya Uryu <uryu.shinya@nies.go.jp>
2020-04-06 16:30:23 +0900

    reshape option

--- R/read_estat_meshpop.R
+++ R/read_estat_meshpop.R
@@ -1,5 +1,7 @@
 #' Read e-Stat mesh population census data
 #' @param file Path to downloaded e-Stat text file
+#' @param is_long If *TRUE*, make the population category a single variable
+#' and make the values of the counterpart population independent.
 #' @param mesh_size Currently, only 0.5 (500m mesh)
 #' @export
 read_estat_meshpop <- function(file, is_long = FALSE, mesh_size = 0.5) {
@@ -12,13 +14,14 @@ read_estat_meshpop <- function(file, is_long = FALSE, mesh_size = 0.5) {
         stringr::str_replace("[[:space:]]", "_") %>%
         stringi::stri_trans_nfkc() %>%
         stringr::str_replace("~", "-"))
-  d %>%
+  d <-
+    d %>%
     tibble::as_tibble() %>%
     dplyr::slice(-1) %>%
     purrr::set_names(c(names(col_vars[seq.int(4)]),
                        purrr::flatten_chr(col_vars[seq.int(5, length(col_vars))]))) %>%
-    dplyr::mutate_all(~ na_if(., "*")) %>%
-    dplyr::mutate_all(~ na_if(., "")) %>%
+    dplyr::mutate_all(~ dplyr::na_if(., "*")) %>%
+    dplyr::mutate_all(~ dplyr::na_if(., "")) %>%
     utils::type.convert() %>%
     dplyr::mutate_at(dplyr::vars(seq.int(3)), as.character)
   if (is_long == TRUE) {
